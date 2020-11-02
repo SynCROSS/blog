e214456eb01e751b504c691718ed7a16d6425c05
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-03 08:29:14 +0900

    Update Tags.js

--- blog-frontend/src/components/common/Tags.js
+++ blog-frontend/src/components/common/Tags.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'C:/Users/kuuha/AppData/Local/Yarn/Data/global/node_modules/styled-components';
-import { Link } from 'C:/Users/kuuha/AppData/Local/Yarn/Data/global/node_modules/react-router-dom';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const TagsBlock = styled.div`
   margin-top: 0.5rem;
