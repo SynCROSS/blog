d76d183336b783560d09945512981f86235ca2dd
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-13 21:47:55 +0900

    Update index.js

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,18 +1,10 @@
 // import Router from 'koa-router';
-// import * as postsCtrl from './posts.ctrl';
 const Router = require('koa-router');
+// import * as postsCtrl from './posts.ctrl';
 const postCtrl = require('./posts.ctrl');
 const checkLoggedIn = require('../../lib/checkLoggedIn');
 
 const posts = new Router();
-
-// const printInfo = (ctx) => {
-//   ctx.body = {
-//     method: ctx.method,
-//     path: ctx.path,
-//     params: ctx.params,
-//   };
-// };
 
 posts.get('/', postCtrl.list);
 posts.post('/', checkLoggedIn, postCtrl.write);
@@ -23,19 +15,7 @@ post.get('/', postCtrl.read);
 post.delete('/', checkLoggedIn, postCtrl.checkOwnPost, postCtrl.remove);
 post.patch('/', checkLoggedIn, postCtrl.checkOwnPost, postCtrl.update);
 
-// posts.get('/:id', postCtrl.checkObjectId, postCtrl.read);
-// posts.delete('/:id', postCtrl.checkObjectId, postCtrl.remove);
-// // posts.put('/:id', postCtrl.replace);
-// posts.patch('/:id', postCtrl.checkObjectId, postCtrl.update);
-
 posts.use('/:id', postCtrl.getPostById, post.routes());
-
-// posts.get('/', printInfo);
-// posts.post('/', printInfo);
-// posts.get('/:id', printInfo);
-// posts.delete('/:id', printInfo);
-// posts.put('/:id', printInfo);
-// posts.patch('/:id', printInfo);
 
 // export default posts;
 module.exports = posts;
