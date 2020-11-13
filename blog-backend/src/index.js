409d811e32c262120eaf07cdaca6e634cad297e7
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-13 21:45:04 +0900

    Update index.js

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,4 @@
-// const u5e = require('abrequire');s
 require('dotenv').config();
-// const url = require('url');
-// const Koa = import(
-//   url.pathToFileURL(
-//     'koa-router'
-//   )
-// );
 
 // import Koa from 'koa';
 // import Router from 'koa-router';
@@ -24,7 +17,6 @@ const send = require('koa-send');
 const api = require('./api');
 const jwtMiddleware = require('./lib/jwtMiddleware');
 // Testing for data injection(?)
-// const createFakeData = require('./createFakeData');
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -45,19 +37,6 @@ router.use('/api', api.routes());
 
 app.use(bodyParser());
 app.use(jwtMiddleware);
-// koaRouter.get('/', (ctx) => {
-//   ctx.body = 'Home';
-// });
-
-// koaRouter.get('/about/:name?', (ctx) => {
-//   const { name } = ctx.params;
-//   ctx.body = name ? `About ${name}` : 'About';
-// });
-
-// koaRouter.get('/posts', (ctx) => {
-//   const { id } = ctx.query;
-//   ctx.body = id ? `Posts #${id}` : 'This Post id is not exist';
-// });
 
 app.use(router.routes()).use(router.allowedMethods());
 
@@ -73,29 +52,3 @@ const port = PORT || 4000;
 app.listen(port, () => {
   console.log('Server is Listening to port %d', port);
 });
-// app.use(async (ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(1);
-//   if (ctx.query.authorized !== '1') {
-//     ctx.status = 401;
-//     return;
-//   }
-//   // next().then(() => {
-//   //   console.log('END');
-//   // });
-//   await next();
-//   console.log('END');
-// });
-
-// app.use((ctx, next) => {
-//   console.log(2);
-//   next();
-// });
-
-// app.use((ctx) => {
-//   ctx.body = 'Hello Koa';
-// });
-
-// app.listen(4000, () => {
-//   console.log('Server is Listening to port 4000');
-// });
